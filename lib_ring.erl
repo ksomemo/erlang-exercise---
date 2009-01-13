12b276beea0b90e7ae91e5d6826882a71fef1267
tkyk <i@tkyk.name>
2009-01-13 22:07:56 +0900

    removed rpc/2
    removed message sender in loop

--- lib_ring.erl
+++ lib_ring.erl
@@ -9,31 +9,28 @@
 -define(TRACE(TEMPLATE, ARGS), void).
 -endif.
 
-rpc(Pid, Request) ->
-    Pid ! {self(), Request},
-    receive
-	{Pid, Response} ->
-	    Response
-    end.
-
 entryloop(Next)  -> loop(Next).
 loop(_Prev, Next) -> loop(Next).
 
 loop(Next) ->
     Self = self(),
     receive
-	{_From, #payload{first=Self, counter=C, message=Msg}=P} ->
+	#payload{first=Self, counter=C, message=Msg}=P ->
 	    if
 		C > 0 -> ?TRACE("last ~p rotations of ~p~n", [C, Msg]),
-			 Next ! {Self, P#payload{counter=C-1}};
+			 Next ! P#payload{counter=C-1};
 		true  -> ?TRACE("finishi rotations of ~p~n", [Msg]),
 			 P#payload.client ! {Self, Msg}
 	    end;
-	{_From, P} when is_record(P, payload) ->
+	P when is_record(P, payload) ->
 	    ?TRACE("pass ~p to the next node ~p~n", [P#payload.message, Next]),
-	    Next ! {Self, P}
+	    Next ! P
     end,
     loop(Next).
 
 start(Pid, M, Msg) when M >= 0 ->
-    rpc(Pid, #payload{client=self(), counter=M, message=Msg, first=Pid}).
+    Pid ! #payload{client=self(), counter=M, message=Msg, first=Pid},
+    receive
+	{Pid, Response} ->
+	    Response
+    end.
